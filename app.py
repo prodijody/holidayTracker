ee55d5c936e936d470e35f716d9b7565eb6dfad9
Sourcery AI <>
2023-11-29 15:03:55 +0000

    'Refactored by Sourcery'

--- app.py
+++ app.py
@@ -52,65 +52,61 @@ def unauthorized():
 def index():
   if current_user.is_authenticated:
     return redirect(url_for('menu'))
-  else:
-    loginForm = LoginForm()
-    resetPasswordForm = RecoverPasswordForm()
-    if request.method == 'POST' and loginForm.validate_on_submit():
-      escaped_email = escape(loginForm.email.data)
-      user = User.query.filter_by(email=escaped_email).first()
-      if user:
-        if check_password_hash(user.password, loginForm.password.data):
-          login_user(user)
-          return redirect(url_for('menu'))
-        else:
-          flash('Wrong password or email.', 'info')
+  loginForm = LoginForm()
+  resetPasswordForm = RecoverPasswordForm()
+  if request.method == 'POST' and loginForm.validate_on_submit():
+    escaped_email = escape(loginForm.email.data)
+    if user := User.query.filter_by(email=escaped_email).first():
+      if check_password_hash(user.password, loginForm.password.data):
+        login_user(user)
+        return redirect(url_for('menu'))
       else:
-        flash('That email is not registered.','info')
-
-      return redirect(url_for('index'))
-
-    elif request.method == 'POST' and resetPasswordForm.validate_on_submit():
-      email = escape(resetPasswordForm.email.data)
-      user = User.query.filter_by(email=email).first()
-      if user:
-        recover_token = user.get_recover_password_token()
-        url = url_for('recover_password', recover_token=recover_token, _external=True)
-        html = render_template('email_templates/reset_password.html', confirm_url=url)
-        sendEmail(email_subject='Reset your password',
-                  recipients=[email],
-                  email_html=html)
-        flash('An email has been sent with a reset link. Follow the link to reset your password within the next 10 minutes.','info')
-      else:
-        flash('That email is not registered.','info')
-      return redirect(url_for('index'))
+        flash('Wrong password or email.', 'info')
     else:
-      return render_template('public/index.html', form=loginForm, form2=resetPasswordForm)
+      flash('That email is not registered.','info')
+
+    return redirect(url_for('index'))
+
+  elif request.method == 'POST' and resetPasswordForm.validate_on_submit():
+    email = escape(resetPasswordForm.email.data)
+    if user := User.query.filter_by(email=email).first():
+      recover_token = user.get_recover_password_token()
+      url = url_for('recover_password', recover_token=recover_token, _external=True)
+      html = render_template('email_templates/reset_password.html', confirm_url=url)
+      sendEmail(email_subject='Reset your password',
+                recipients=[email],
+                email_html=html)
+      flash('An email has been sent with a reset link. Follow the link to reset your password within the next 10 minutes.','info')
+    else:
+      flash('That email is not registered.','info')
+    return redirect(url_for('index'))
+  else:
+    return render_template('public/index.html', form=loginForm, form2=resetPasswordForm)
 
 
 @app.route('/recover_password/<recover_token>', methods=['GET','POST'])
 def recover_password(recover_token):
   if current_user.is_authenticated:
     return redirect(url_for('menu'))
-  else:
-    verify_token = User.verify_reset_password_token(recover_token)
-    if not verify_token:
-      flash('This reset link is invalid or has expired. Request a new one.', 'info')
+  verify_token = User.verify_reset_password_token(recover_token)
+  if not verify_token:
+    flash('This reset link is invalid or has expired. Request a new one.', 'info')
+    return redirect(url_for('index'))
+
+  form = ResetPasswordForm()
+  if request.method == 'POST' and form.validate_on_submit():
+
+    if form.password.data == form.password2.data:
+      user = verify_token
+      hashed_password = generate_password_hash(form.password.data, method='sha256')
+      user.password = hashed_password
+      db.session.commit()
+      flash('Your password has been updated. You may now login.', 'success')
       return redirect(url_for('index'))
-
-    form = ResetPasswordForm()
-    if request.method == 'POST' and form.validate_on_submit():
-
-      if form.password.data != form.password2.data:
-        flash('Those passwords did not match. Try again!','danger')
-        return redirect(url_for('recover_password', recover_token=recover_token))
-      else:
-        user = verify_token
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        user.password = hashed_password
-        db.session.commit()
-        flash('Your password has been updated. You may now login.', 'success')
-        return redirect(url_for('index'))
-    return render_template('public/reset_password.html', form=form, token=recover_token)
+    else:
+      flash('Those passwords did not match. Try again!','danger')
+      return redirect(url_for('recover_password', recover_token=recover_token))
+  return render_template('public/reset_password.html', form=form, token=recover_token)
 
 
 # Menu route
@@ -121,7 +117,10 @@ def menu():
     pending_requests = len(HolidayRequest.query.filter_by(status='Pending').all())
 
     if pending_requests > 0:
-      flash('You have <strong>{}</strong> holiday request(s) waiting for un update. <a href="{}">Click here</a>.'.format(pending_requests, url_for('admin')),'info')
+      flash(
+          f"""You have <strong>{pending_requests}</strong> holiday request(s) waiting for un update. <a href="{url_for('admin')}">Click here</a>.""",
+          'info',
+      )
 
   return render_template('protected/menu.html')
 
@@ -131,7 +130,12 @@ def menu():
 @login_required
 def calendar():
   requests = HolidayRequest.query.filter_by(status='Approved').order_by(HolidayRequest.date_from.asc()).all()
-  requests_for_calendar = [{'title' : i.get_user().name + ' ' + i.get_user().surname, 'start' : str(i.date_from), 'end' : str(i.date_to), 'url': url_for('open_request', request_id=i.id)} for i in requests]
+  requests_for_calendar = [{
+      'title': f'{i.get_user().name} {i.get_user().surname}',
+      'start': str(i.date_from),
+      'end': str(i.date_to),
+      'url': url_for('open_request', request_id=i.id),
+  } for i in requests]
   return render_template('protected/calendar.html', requests=requests, requests_for_calendar=requests_for_calendar)
 
 
@@ -164,7 +168,7 @@ def request_holidays():
     sendEmail(email_subject='Your holiday request', recipients=[current_user.email], email_html=html)
 
     # email manager
-    user = current_user.name + ' ' + current_user.surname
+    user = f'{current_user.name} {current_user.surname}'
     html2 = render_template('email_templates/admin_holiday_request.html', date_from=date_from, date_to=date_to, comment=comment, user=user)
     ADMIN_ROLE = SystemRole.query.filter_by(name='Admin').first().id
     THE_ADMIN = User.query.filter_by(system_role=ADMIN_ROLE).first().email
@@ -180,8 +184,7 @@ def request_holidays():
 @login_required
 def open_request(request_id):
   req = escape(request_id)
-  request_exists = HolidayRequest.query.get(req)
-  if request_exists:
+  if request_exists := HolidayRequest.query.get(req):
     if request_exists.user_id == current_user.id or current_user.is_admin():
       form = RequestHolidaysForm(obj=request_exists)
 
@@ -198,7 +201,7 @@ def open_request(request_id):
       flash('Noting to see here, details of that holiday request are private.','info')
       return redirect(url_for('calendar'))
   else:
-    flash('That request id {} is invalid.'.format(request_id),'danger')
+    flash(f'That request id {request_id} is invalid.', 'danger')
     return redirect(url_for('calendar'))
 
 
@@ -226,10 +229,12 @@ def admin_user(user_id):
     return redirect(url_for('menu'))
 
   user_id = escape(user_id)
-  user = User.query.get(user_id)
-  if user:
+  if user := User.query.get(user_id):
     form2 = UpdateUserForm(obj=user)
-    form2.system_role.choices = [(i.id, str(i.id) + ') ' + i.name) for i in SystemRole.query.order_by(SystemRole.id.desc()).all()]
+    form2.system_role.choices = [
+        (i.id, f'{str(i.id)}) {i.name}')
+        for i in SystemRole.query.order_by(SystemRole.id.desc()).all()
+    ]
 
     form = UpdateHolidaysRequest()
     form.request.choices = [(0, 'Select one')] + [(i.id, i.id) for i in HolidayRequest.query.filter_by(user_id=user_id).all()]
@@ -242,7 +247,6 @@ def admin_user(user_id):
       find_request = HolidayRequest.query.get(request_id)
       if not find_request or status not in ['Approved', 'Cancelled', 'Pending','Declined']:
         flash('Please choose from the list a request ID and a status.','danger')
-        return redirect(url_for('admin'))
       else:
         find_request.status = status
         find_request.manager_comment = comment
@@ -252,8 +256,7 @@ def admin_user(user_id):
         html = render_template('email_templates/request_updated.html', date_from=find_request.date_from, date_to=find_request.date_to, comment=find_request.comment, status=find_request.status, manager_comment=find_request.manager_comment)
         sendEmail(email_subject='Update to your holiday request', recipients=[user.email], email_html=html)
 
-        return redirect(url_for('admin'))
-
+      return redirect(url_for('admin'))
     return render_template('protected/admin/admin_user.html', user=user, form=form, form2=form2)
   else:
     flash('User id {user_id} does not exist.'.format(user_id=user_id), 'danger')
@@ -268,10 +271,12 @@ def update_user(user_id):
     return redirect(url_for('menu'))
 
   user_id = escape(user_id)
-  user = User.query.get(user_id)
-  if user:
+  if user := User.query.get(user_id):
     form2 = UpdateUserForm()
-    form2.system_role.choices = [(i.id, str(i.id) + ') ' + i.name) for i in SystemRole.query.order_by(SystemRole.id.desc()).all()]
+    form2.system_role.choices = [
+        (i.id, f'{str(i.id)}) {i.name}')
+        for i in SystemRole.query.order_by(SystemRole.id.desc()).all()
+    ]
     if request.method == 'POST' and form2.validate_on_submit():
       user.name = escape(form2.name.data)
       user.surname = escape(form2.surname.data)
@@ -296,7 +301,10 @@ def add_user():
 
   existing_users = User.query.all()
   addUserForm = AddUserForm()
-  addUserForm.system_role.choices = [(i.id, str(i.id) + ') ' + i.name) for i in SystemRole.query.order_by(SystemRole.id.desc()).all()]
+  addUserForm.system_role.choices = [
+      (i.id, f'{str(i.id)}) {i.name}')
+      for i in SystemRole.query.order_by(SystemRole.id.desc()).all()
+  ]
   if request.method == 'POST' and addUserForm.validate_on_submit():
     # escape form input
     name = escape(addUserForm.name.data)
@@ -305,9 +313,7 @@ def add_user():
     system_role = escape(addUserForm.system_role.data)
     holidays_quota = escape(addUserForm.holidays_quota.data)
 
-    # check email exists
-    email_exists = User.query.filter_by(email=email).first()
-    if email_exists:
+    if email_exists := User.query.filter_by(email=email).first():
       flash('That email is already registered.','danger')
       return redirect(url_for('add_user'))
 
